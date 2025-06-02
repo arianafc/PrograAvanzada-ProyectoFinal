506eb415a88092278c8bdf61be662ffb15c863ed
arianafc <arianafc1108@gmail.com>
2025-06-01 21:47:30 -0600

    Layout Externo

---
+++ Scripts/script.js
@@ -0,0 +1,12 @@
+const hideToggle = document.querySelector(".hide-toggle");
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+hideToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("hide");
+});
+menuToggle.addEventListener("click", () => {
+  if (navLinks.classList.contains("hide")) {
+    navLinks.classList.remove("hide");
+    navLinks.classList.toggle("show");
+  }
+});
