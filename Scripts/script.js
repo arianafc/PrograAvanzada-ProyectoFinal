428b85c49e31b7abc0f4b6399c962231f31e6065
arianafc <arianafc1108@gmail.com>
2025-06-02 23:23:04 -0600

    update

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,12 +1,23 @@
-const hideToggle = document.querySelector(".hide-toggle");
-const menuToggle = document.querySelector(".menu-toggle");
-const navLinks = document.querySelector(".nav-links");
-hideToggle.addEventListener("click", () => {
-  navLinks.classList.toggle("hide");
-});
-menuToggle.addEventListener("click", () => {
-  if (navLinks.classList.contains("hide")) {
-    navLinks.classList.remove("hide");
-    navLinks.classList.toggle("show");
-  }
+
+
+$(document).ready(function () {
+
+	const hideToggle = document.querySelector(".hide-toggle");
+	const menuToggle = document.querySelector(".menu-toggle");
+	const navLinks = document.querySelector(".nav-links");
+	hideToggle.addEventListener("click", () => {
+		navLinks.classList.toggle("hide");
+	});
+	menuToggle.addEventListener("click", () => {
+		if (navLinks.classList.contains("hide")) {
+			navLinks.classList.remove("hide");
+			navLinks.classList.toggle("show");
+		}
+
+	});
+
+
+
+
+
 });
